c26d81a4e868d0ce105f965ab685989d50de756b
soomroshoaib <soomroshoaib178@gmail.com>
2023-09-06 01:13:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log("science")
+
+// comparions opertors 
+//== ===
+
+// console.log( 5 === "5")
+
+// console.log(5 != 6)
+
+// const num = 8;
+// const num2 = 6
+
+// console.log(num <= num2)
+
+
+
+const num = +prompt("enter your age  ..... ")
+
+if(num >= 18){
+    console.log("welcome ....jnb ")
+}
